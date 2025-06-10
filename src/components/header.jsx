041ba175f2ed26d5e7eb9ffc4f83928fdcbece09
Joshua <mutenjejoshua@gmail.com>
2025-06-10 12:54:28 +0200

    Fixed Hamburger toggle button

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,35 +1,43 @@
-function Header (){
-    return (
-        <>
-        <nav id="desktop-nav">
+import { useState } from 'react';
+
+function Header() {
+  const [menuOpen, setMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setMenuOpen(!menuOpen);
+  };
+
+  return (
+    <>
+      <nav id="desktop-nav">
         <div className="logo">Takundanashe J Mutenje</div>
         <div>
-            <ul className="nav-links">
-                <li><a href="#about">About</a></li>
-                <li><a href="#experience">Experience</a></li>
-                <li><a href="#projects">Projects</a></li>
-                <li><a href="#contact">Contact</a></li>
-            </ul>
-    </div>
-    </nav>
-    <nav id="hamburger-nav">
-      <div className="logo">Takundanashe J Mutenje</div>
-      <div className="hamburger-menu">
-        <div className="hamburger-icon" onclick="toggleMenu()">
-          <span></span>
-          <span></span>
-          <span></span>
+          <ul className="nav-links">
+            <li><a href="#about">About</a></li>
+            <li><a href="#experience">Experience</a></li>
+            <li><a href="#projects">Projects</a></li>
+            <li><a href="#contact">Contact</a></li>
+          </ul>
         </div>
-        <div className="menu-links">
-          <li><a href="#about" onclick="toggleMenu()">About</a></li>
-          <li><a href="#experience" onclick="toggleMenu()">Experience</a></li>
-          <li><a href="#projects" onclick="toggleMenu()">Projects</a></li>
-          <li><a href="#contact" onclick="toggleMenu()">Contact</a></li>
+      </nav>
+      <nav id="hamburger-nav">
+        <div className="logo">Takundanashe J Mutenje</div>
+        <div className="hamburger-menu">
+          <div className="hamburger-icon" onClick={toggleMenu}>
+            <span></span>
+            <span></span>
+            <span></span>
+          </div>
+          <div className={`menu-links ${menuOpen ? "open" : ""}`}>
+            <li><a href="#about" onClick={toggleMenu}>About</a></li>
+            <li><a href="#experience" onClick={toggleMenu}>Experience</a></li>
+            <li><a href="#projects" onClick={toggleMenu}>Projects</a></li>
+            <li><a href="#contact" onClick={toggleMenu}>Contact</a></li>
+          </div>
         </div>
-      </div>
-    </nav>
-        </>
-    );
+      </nav>
+    </>
+  );
 }
 
 export default Header;
